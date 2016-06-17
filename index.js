ac0fc55177b5e3767d5796918d2a544204a7d01a
Peter Andreas Moelgaard <admin@petermolgaard.com>
2016-06-17 16:39:31 +0700

    relaying more correct status and errors

--- index.js
+++ index.js
@@ -4,37 +4,44 @@ var ensureHttp = require('ensure-http');
 
 module.exports = function (sails, swagger) {
 
-  _.each(_.keys(swagger.paths), function (path) {
+    _.each(_.keys(swagger.paths), function (path) {
 
-    var pathDef = swagger.paths[path];
+        var pathDef = swagger.paths[path];
 
-    _.each(_.keys(pathDef), function (vertex) {
+        _.each(_.keys(pathDef), function (vertex) {
 
-      var vertexDef = pathDef[vertex];
+            var vertexDef = pathDef[vertex];
 
-      var route = vertex.toUpperCase() + ' ' + ( swagger.basePath || '' ) + '' + path;
+            var route = vertex.toUpperCase() + ' ' + ( swagger.basePath || '' ) + '' + path;
 
-      route = route.replace(/{/g, ':').replace(/}/g, '');
+            route = route.replace(/{/g, ':').replace(/}/g, '');
 
-      console.log('Binding...' + route);
+            console.log('Binding...' + route);
 
-      sails.router.bind(route, function (req, res, next) {
+            sails.router.bind(route, function (req, res, next) {
 
-        var targetUrlTemplate = ensureHttp(swagger.host + ( swagger.basePath || '' ) + path);
-        var templateFn = _.template(targetUrlTemplate, {
-          interpolate: /{([\s\S]+?)}/g
-        });
+                var targetUrlTemplate = ensureHttp(swagger.host + ( swagger.basePath || '' ) + path);
+                var templateFn = _.template(targetUrlTemplate, {
+                    interpolate: /{([\s\S]+?)}/g
+                });
 
-        var params = req.allParams();
+                var params = req.allParams();
 
-        var targetUrl = templateFn(params);
+                var targetUrl = templateFn(params);
 
-        var reqOut = {url: targetUrl, method: vertex, body: ( req.body || null ), json: true};
-        request(reqOut, function (err, message, body) {
+                var reqOut = {url: targetUrl, method: vertex, body: ( req.body || null ), json: true};
+                request(reqOut, function (err, message, body) {
 
-          res.ok(body);
+                    if (!err) {
+                        res
+                            .status(message.statusCode)
+                            .send(body);
+                    }
+                    else {
+                        res.error(err);
+                    }
+                })
+            })
         })
-      })
     })
-  })
 }
